fdca0c072cebe7b6f704dab3f717749bfd727c2f
deymar666 <deymar666@outlook.com>
2023-04-12 18:14:36 -0400

    formulario

---
+++ destino.php
@@ -0,0 +1,56 @@
+<?php
+  echo $_POST ['nombre'];
+  echo'<br>';
+  echo $_POST ['apellido'];
+  echo'<br>';
+  echo $_POST ['genero'];
+  echo'<br>';
+  echo $_POST ['ciudad'];
+  echo'<br>';
+  if(isset($_POST['lenguaje1']))
+  {
+  echo $_POST['lenguaje1'];
+  echo'<br>';
+  }
+  if(isset($_POST['lenguaje2']))
+  {
+  echo $_POST['lenguaje2'];
+  echo'<br>';
+  }
+  if(isset($_POST['lenguaje3']))
+  {
+  echo $_POST['lenguaje3'];
+  echo'<br>';
+  }
+  echo $_POST ['color'];
+  echo'<br>';
+  echo $_POST ['nacimiento'];
+  echo'<br>';
+  echo $_POST ['cumple'];
+  echo'<br>';
+  echo $_POST ['fechaexamen'];
+  echo'<br>';
+  echo $_POST ['mes'];
+  echo'<br>';
+  echo $_POST ['horaIngreso'];
+  echo'<br>';
+  echo $_POST ['vacaciones'];
+  echo'<br>';
+  echo $_POST ['correo'];
+  echo'<br>';
+  echo $_POST ['sitioweb'];
+  echo'<br>';
+  echo $_POST ['saberhtml'];
+  echo'<br>';
+  echo $_POST ['saberhtml5'];
+  echo'<br>';
+  echo $_POST ['clave'];
+  echo'<br>';
+  
+ 
+ 
+  
+
+  
+
+?>
---
+++ script.php
@@ -0,0 +1,23 @@
+<?php
+echo $_POST['nombre'];
+echo'<br>';
+echo $_POST['contraseña'];
+echo'<br>';
+echo $_POST['datosPersonales'];
+echo'<br>';
+echo $_POST['ciudad'];
+echo'<br>';
+echo $_POST['nacimiento'];
+echo'<br>';
+echo $_POST['correo'];
+echo'<br>';
+echo $_POST['lenguaje1'];
+echo'<br>';
+echo $_POST['lenguaje2'];
+echo'<br>';
+echo $_POST['lenguaje3'];
+echo'<br>';
+echo $_POST['color'];
+echo'<br>';
+
+?>
